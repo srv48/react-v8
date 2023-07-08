f3543525aa308739ec1e556164c9c7f3ac39f2ec
Sourav B <sourav@email.com>
2023-07-08 20:29:34 +0530

    elint config

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,14 @@
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+import React from "react";
+import ReactDOM from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Tommy",
-      animal: "Dog",
-      breed: "Cute",
-    }),
-    React.createElement(Pet, {
-      name: "Sourav",
-      animal: "Human",
-      breed: "Normal",
-    }),
-  ]);
+  return (
+    <div>
+      <Pet name="Tommy" animal="Dog" breed="Cute" />
+      <Pet name="Sourav" animal="Human" breed="Normal" />
+    </div>
+  );
 };
 
 const container = document.getElementById("root");
