558a681a64bfc18e63a7929beb8bbba74eeae3e8
Sourav B <sourav@email.com>
2023-07-08 23:29:43 +0530

    add hooks

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 
 const ANIMALS = ["dog", "bird", "cat", "rabbit", "reptile"];
+const BREEDS = ["indog"];
 const SearchParams = () => {
   let [location, setLocation] = useState("");
   let [animal, setAnimal] = useState("");
+  let [breed, setBreed] = useState("");
 
   return (
     <div className="search-params">
@@ -22,10 +24,28 @@ const SearchParams = () => {
           <select
             id="animal"
             value={animal}
-            onChange={(event) => setAnimal(event.target.value)}
+            onChange={(event) => {
+              setAnimal(event.target.value);
+              setBreed("");
+            }}
           >
+            <option />
             {ANIMALS.map((animal) => (
               <option key={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(event) => setBreed(event.target.value)}
+            disabled={BREEDS.length === 0}
+          >
+            <option />
+            {BREEDS.map((breed) => (
+              <option key={breed}>{breed}</option>
             ))}
           </select>
         </label>
