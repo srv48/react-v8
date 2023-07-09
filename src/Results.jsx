b4c728e045145508b418f9b7da3041d023b95bc4
Sourav B <sourav@email.com>
2023-07-10 00:56:43 +0530

    component composition

---
+++ src/Results.jsx
@@ -0,0 +1,22 @@
+import Pet from "./Pet";
+
+let Results = ({ pets }) => {
+  return (
+    <div className="search">
+      {!pets.length ? (
+        <h1>No pets found</h1>
+      ) : (
+        pets.map((pet) => (
+          <Pet
+            name={pet.name}
+            animal={pet.animal}
+            breed={pet.breed}
+            key={pet.id}
+          />
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Results;
--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-import Pet from "./Pet";
 import useBreedList from "./useBreedList";
+import Results from "./Results";
 
 const ANIMALS = ["dog", "bird", "cat", "rabbit", "reptile"];
 
@@ -73,14 +73,7 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
-      {pets.map((pet) => (
-        <Pet
-          name={pet.name}
-          animal={pet.animal}
-          breed={pet.breed}
-          key={pet.id}
-        />
-      ))}
+      <Results pets={pets} />
     </div>
   );
 };
